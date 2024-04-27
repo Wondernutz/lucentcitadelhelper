432800e85c91ab8c08753ae4833da5a47b2a4fd6
Brian Lee <brian.lee@ni.com>
2024-04-26 21:26:36 -0500

    track incoming time to next fluctuating current

--- LucentCitadelHelper/LucentCitadelHelper.lua
+++ LucentCitadelHelper/LucentCitadelHelper.lua
@@ -2,7 +2,7 @@ LCH = LCH or {}
 local LCH = LCH
 
 LCH.name     = "LucentCitadelHelper"
-LCH.version  = "0.2.6"
+LCH.version  = "0.2.7"
 LCH.author   = "@Wondernuts, @kabs12"
 LCH.active   = false
 
