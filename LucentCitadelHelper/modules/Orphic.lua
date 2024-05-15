057d1c1883dca4ced524325ba8dd694bc509a964
Kabsi <kabs7320@gmail.com>
2024-05-14 22:33:04 -0300

    Fixed an oopsie

--- LucentCitadelHelper/modules/Orphic.lua
+++ LucentCitadelHelper/modules/Orphic.lua
@@ -28,72 +28,56 @@ function LCH.Orphic.AddMirrorIcons()
     if table.getn(LCH.status.MirrorIconNumber1) == 0 then
       table.insert(LCH.status.MirrorIconNumber1, 
         OSI.CreatePositionIcon(
-          149309,
-          22868,
-          85334,
+          149311,22871,85560,
           "LucentCitadelHelper/icons/1.dds",
           1 * OSI.GetIconSize()))
     end
     if table.getn(LCH.status.MirrorIconNumber2) == 0 then
       table.insert(LCH.status.MirrorIconNumber2, 
         OSI.CreatePositionIcon(
-          151958,
-          22867,
-          87981,
+          151030,22870,86281,
           "LucentCitadelHelper/icons/2.dds",
           1 * OSI.GetIconSize()))
     end
     if table.getn(LCH.status.MirrorIconNumber3) == 0 then
       table.insert(LCH.status.MirrorIconNumber3, 
         OSI.CreatePositionIcon(
-          182466,
-          40391,
-          222635,
+          151808,22869,87932,
           "LucentCitadelHelper/icons/3.dds",
           1 * OSI.GetIconSize()))
     end
     if table.getn(LCH.status.MirrorIconNumber4) == 0 then
       table.insert(LCH.status.MirrorIconNumber4, 
         OSI.CreatePositionIcon(
-          151170,
-          22864,
-          89707, 
+          151052,22869,89768,
           "LucentCitadelHelper/icons/4.dds",
           1 * OSI.GetIconSize()))
     end
     if table.getn(LCH.status.MirrorIconNumber5) == 0 then
       table.insert(LCH.status.MirrorIconNumber5, 
         OSI.CreatePositionIcon(
-          149282,
-          22869,
-          90564,
+          149300,22871,90476,
           "LucentCitadelHelper/icons/5.dds",
           1 * OSI.GetIconSize()))
     end
     if table.getn(LCH.status.MirrorIconNumber6) == 0 then
       table.insert(LCH.status.MirrorIconNumber6, 
         OSI.CreatePositionIcon(
-          147494,
-          22869,
-          89770,
+          147443,22868,89821,
           "LucentCitadelHelper/icons/6.dds",
           1 * OSI.GetIconSize()))
     end
     if table.getn(LCH.status.MirrorIconNumber7) == 0 then
       table.insert(LCH.status.MirrorIconNumber7, 
         OSI.CreatePositionIcon(
-          146618,
-          22867,
-          87932,
+          146755,22869,87935,
           "LucentCitadelHelper/icons/7.dds",
           1 * OSI.GetIconSize()))
     end
     if table.getn(LCH.status.MirrorIconNumber8) == 0 then
       table.insert(LCH.status.MirrorIconNumber8, 
         OSI.CreatePositionIcon(
-          147495,
-          22869,
-          86171,
+          147539,22869,86217,
           "LucentCitadelHelper/icons/8.dds",
           1 * OSI.GetIconSize()))
     end
@@ -103,18 +87,25 @@ end
 function LCH.Orphic.RemoveMirrorIcons()
   LCH.DiscardPositionIconList(LCH.status.MirrorIconNumber1)
   LCH.status.MirrorIconNumber1 = {}
+
   LCH.DiscardPositionIconList(LCH.status.MirrorIconNumber2)
   LCH.status.MirrorIconNumber2 = {}
+
   LCH.DiscardPositionIconList(LCH.status.MirrorIconNumber3)
   LCH.status.MirrorIconNumber3 = {}
+
   LCH.DiscardPositionIconList(LCH.status.MirrorIconNumber4)
   LCH.status.MirrorIconNumber4 = {}
+
   LCH.DiscardPositionIconList(LCH.status.MirrorIconNumber5)
   LCH.status.MirrorIconNumber5 = {}
+
   LCH.DiscardPositionIconList(LCH.status.MirrorIconNumber6)
   LCH.status.MirrorIconNumber6 = {}
+
   LCH.DiscardPositionIconList(LCH.status.MirrorIconNumber7)
   LCH.status.MirrorIconNumber7 = {}
+  
   LCH.DiscardPositionIconList(LCH.status.MirrorIconNumber8)
   LCH.status.MirrorIconNumber8 = {}
 end
